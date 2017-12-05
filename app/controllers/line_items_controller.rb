a2f1a35ac65410ae654bb413f8a708d1e3b1d92b
clehma203 <Christopher_Lehman2@cable.comcast.com>
2017-12-05 09:30:38 -0700

    Depot Task E

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@ class LineItemsController < ApplicationController
   include CurrentCart
 
   before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_line_item, only: %i[show edit update destroy]
 
   # GET /line_items
   # GET /line_items.json
@@ -28,12 +28,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-    # @line_item = LineItem.new(line_item_params)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -75,6 +74,6 @@ class LineItemsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def line_item_params
-    params.require(:line_item).permit(:product_id, :cart_id)
+    params.require(:line_item).permit(:product_id)
   end
 end
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -16,8 +16,8 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create cart" do
-    assert_difference('Cart.count') do
-      post carts_url, params: { cart: {  } }
+    assert_difference("Cart.count") do
+      post carts_url, params: { cart: {} }
     end
 
     assert_redirected_to cart_url(Cart.last)
@@ -34,15 +34,18 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update cart" do
-    patch cart_url(@cart), params: { cart: {  } }
+    patch cart_url(@cart), params: { cart: {} }
     assert_redirected_to cart_url(@cart)
   end
 
   test "should destroy cart" do
-    assert_difference('Cart.count', -1) do
+    post line_items_url, params: { product_id: products(:ruby).id }
+    @cart = Cart.find(session[:cart_id])
+
+    assert_difference("Cart.count", -1) do
       delete cart_url(@cart)
     end
 
-    assert_redirected_to carts_url
+    assert_redirected_to store_index_url
   end
 end
