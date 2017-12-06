050a4543c10b6c75f79e6219cecd3c24a8f6498d
clehma203 <Christopher_Lehman2@cable.comcast.com>
2017-12-06 15:55:10 -0700

    Depot Task H

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,5 +14,6 @@ module Depot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.filter_parameters += [:credit_card_number]
   end
 end
